eb33e1f4246b742ec041e3182d2ce641664cff0e
Jatin Rungta <jatinrungta27@gmai.com>
2018-10-16 19:28:17 +0530

    Add target group for Load Balancer

--- main.tf
+++ main.tf
@@ -8,26 +8,6 @@ resource "aws_key_pair" "mera" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDb2fUlXI4q9EPI+IutLoztht86L3WtApYkGe38MYxm5HobEJ8OK3q20cy5b1ExzCNzP8JznJCFUW4NaqpZq46KJSqAT1NaxG/D4H6pVJReRLCjfDh3zpSs0nwUWWPYRANj2tDRhXK557b1btxW9PQ9IGYpJ3E/rLubn4CHTwrwTDMVT/qd1nPrgwF5yr7UL468RumSnsAcmH3ZJMypBDRaTMpkpnxSUQjWH8/Uf3nqXkK4oprKYOIcZOfcmRGOjpFFjOutG2aDabmfhoXgeWvwzlqMmPiFhhmTvQrJwDcN4Sq2rj8V23N5xihm42LveeQEwpqyJCUt94UbaRlf449v student@a627953-linux"
   key_name = "mera"
 }
-
-# resource "aws_instance" "example" {
-  
-#   count         = 2
-#   ami           = "ami-0b9a214f40c38d5eb"
-#   instance_type = "t2.micro"
-#   vpc_security_group_ids = ["${aws_security_group.publica.id}"]
-#   iam_instance_profile = "${data.aws_iam_role.ecsaccess.name}"
-#   user_data = <<-EOF
-#             #!/bin/bash
-#             echo 'ECS_CLUSTER=${aws_ecs_cluster.ecsc.name}' >> /etc/ecs/ecs.config
-#             EOF
-
-#   key_name = "mera"
-
-#   tags {
-#     name = "cluster_instance"
-#   }
-# }
-
 # resource "aws_ecs_service" "ecss"{
 #   name = "nginx"
 #   task_definition = ""
@@ -58,6 +38,12 @@ resource "aws_security_group" "docker_alb" {
     protocol ="tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 8
+    to_port = 0
+    protocol ="icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port = 0
     to_port = 65535
@@ -74,6 +60,7 @@ resource "aws_security_group" "publica"{
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
   ingress {
     from_port = 0
     to_port = 65535
@@ -126,7 +113,7 @@ resource "aws_autoscaling_group" "asg1" {
   health_check_grace_period = 30
   default_cooldown = 30
   health_check_type = "ELB"
-  target_group_arns = ["${aws_lb.docker-alb.arn}"]
+  target_group_arns = ["${aws_lb_target_group.docker-alb-tg.id}"]
   min_size = 2
   max_size = 5
   lifecycle {
@@ -150,13 +137,22 @@ resource "aws_lb" "docker-alb" {
 }
 
 resource "aws_lb_listener" "docker-alb-lis" {
-  load_balancer_arn = "${aws_lb.docker-alb.arn}"
+  load_balancer_arn = "${aws_lb.docker-alb.id}"
   port = "80"
 
   default_action{
     type = "forward"
-    target_group_arn = ""
+    target_group_arn = "${aws_lb_target_group.docker-alb-tg.id}"
   }
 
   
 }
+
+resource "aws_lb_target_group" "docker-alb-tg" {
+  name = "docker-lb-grp"
+  port = "8080"
+  protocol ="HTTP"
+  vpc_id = "${aws_default_vpc.default_vpc.id}"
+
+}
+
